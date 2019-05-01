97c7ec35f438254d7c9989ab0a6fda6d070d8766
Jahenzebkhan <50129717+Jahenzebkhan@users.noreply.github.com>
2019-05-01 16:33:55 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,90 +1,97 @@
 <?php
-session_start();
+require("includes/common.php");
+
+// Redirects the user to products page if he/she is logged in.
+
+  header('location: products.php');
+
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <link rel="shortcut icon" href="img/lifestyleStore.png" />
-        <title>Lifestyle Store</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- latest compiled and minified CSS -->
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
-        <!-- jquery library -->
-        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
-        <!-- Latest compiled and minified javascript -->
-        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-        <!-- External CSS -->
-        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Welcome | E-Khareedari </title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
     </head>
-    <body>
-        <div>
-           <?php
-            require 'header.php';
-           ?>
-           <div id="bannerImage">
-               <div class="container">
-                   <center>
-                   <div id="bannerContent">
-                       <h1>We sell lifestyle.</h1>
-                       <p>Flat 40% OFF on all premium brands.</p>
-                       <a href="products.php" class="btn btn-danger">Shop Now</a>
-                   </div>
-                   </center>
-               </div>
-           </div>
-           <div class="container">
-               <div class="row">
-                   <div class="col-xs-4">
-                       <div  class="thumbnail">
-                           <a href="products.php">
-                                <img src="img/camera.jpg" alt="Camera">
-                           </a>
-                           <center>
+    <body style="padding-top: 50px;">
+        <!-- Header -->
+        <?php
+        include 'includes/header.php';
+        ?>
+        <!--Header end-->
+
+        <div id="content">
+            <!--Main banner image-->
+            <div id = "banner_image">
+                <div class="container">	
+                    <center>
+                        <div id="banner_content">
+                            <h1>We sell Dailylife items.</h1>
+                            <p>Flat 40% OFF on premium brands </p>
+                            <br/>
+                            <a  href="products.php" class="btn btn-danger btn-lg active">Shop Now</a>
+                        </div>
+                    </center>
+                </div>
+            </div>
+            <!--Main banner image end-->
+
+            <!--Item categories listing-->
+            <div class="container">
+                <div class="row text-center" id="item_list">
+                    <div class="col-sm-4">
+                        <a href="products.php#cameras" >
+                            <div class="thumbnail">
+                                <img src="img/1.jpg" alt="">
                                 <div class="caption">
-                                        <p id="autoResize">Cameras</p>
-                                        <p>Choose among the best available in the world.</p>
+                                    <h3>Cameras</h3>
+                                    <p>Choose among the best available in the world.</p>
                                 </div>
-                           </center>
-                       </div>
-                   </div>
-                   <div class="col-xs-4">
-                       <div class="thumbnail">
-                           <a href="products.php">
-                               <img src="img/watch.jpg" alt="Watch">
-                           </a>
-                           <center>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="products.php#watches" >
+                            <div class="thumbnail">
+                                <img src="img/10.jpg" alt="">
                                 <div class="caption">
-                                    <p id="autoResize">Watches</p>
+                                    <h3>Watches</h3>
                                     <p>Original watches from the best brands.</p>
                                 </div>
-                           </center>
-                       </div>
-                   </div>
-                   <div class="col-xs-4">
-                       <div class="thumbnail">
-                           <a href="products.php">
-                               <img src="img/shirt.jpg" alt="Shirt">
-                           </a>
-                           <center>
-                               <div class="caption">
-                                   <p id="autoResize">Shirts</p>
-                                   <p>Our exquisite collection of shirts.</p>
-                               </div>
-                           </center>
-                       </div>
-                   </div>
-               </div>
-           </div>
-            <br><br> <br><br><br><br>
-           <footer class="footer"> 
-               <div class="container">
-               <center>
-                   <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
-                   
-               </center>
-               </div>
-           </footer>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="products.php#shirts" >
+                            <div class="thumbnail">
+                                <img src="img/13.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Shirts</h3>
+                                    <p>Our exquisite collection of shirts.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+            <!--Item categories listing end-->
         </div>
-    </body>
+        
+        <!--Footer-->
+        <?php
+        include 'includes/footer.php';
+        ?>
+        <!--Footer end-->
+   
+    </body> 
 </html>
